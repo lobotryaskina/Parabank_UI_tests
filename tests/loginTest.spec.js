6725fa71e47ee9124af0bb9baf3a9ce521d28dac
lobotryaskina <sofiaryaskina@gmail.com>
2023-10-10 13:32:07 +0300

    home page, login and registration ui tests

---
+++ tests/loginTest.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test'
+
+test("Home page login availability", async ({ page }) => {
+    await page.goto("index.htm");
+    await expect(page.locator('input[name="username"]')).toBeVisible;
+    await expect(page.locator('input[name="password"]')).toBeVisible;
+    await expect(page.locator('value=Log In')).toBeVisible;
+});
+
+test("Successful login", async ({ page }) => {
+    await page.goto("index.htm");
+    await page.locator('input[name="username"]').fill('testUsErr');
+    await page.locator('input[name="password"]').fill('1355');
+    await page.click('input.button');
+    await expect(page.getByText('Account Services')).toBeVisible;
+    await expect(page.locator('p.smallText')).toContainText('Welcome')
+});
+
+test("Empty credentials", async ({ page }) => {
+    await page.goto("index.htm");
+    await page.click('input.button');
+    await expect(page.locator('p.error')).toContainText('Please enter a username and password.')
+});
